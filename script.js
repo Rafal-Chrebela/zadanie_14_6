178556897bc4ab0c5e525ec66052d718c24ac57f
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-03-27 00:46:24 +0200

    Add index.html and script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+var Counter = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
+
+  increment: function() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+
+  decrement: function() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
+
+  render: function() {
+    return React.createElement(
+      "div",
+      {},
+      React.createElement("button", { onClick: this.increment }, "Dodaj"),
+      React.createElement("button", { onClick: this.decrement }, "Odejmij"),
+      React.createElement("span", {}, "Stan licznika: " + this.state.counter)
+    );
+  },
+
+  componentWillMount: function() {
+    console.log(
+      "Komponent wywołany przed renderowaniem. Przygotowanie do liczenia"
+    );
+  },
+
+  componentDidMount: function() {
+    console.log("Pobieranie danych z serwera");
+  },
+
+  componentWillReceiveProps: function() {
+    console.log("Aktualizacja po wprowadzeniu zmian");
+  },
+
+  ShouldComponentUpdate: function() {
+    console.log(
+      "Wyjście z aktualizacji jeśli nie ma potrzeby dalszego renderowania"
+    );
+  },
+
+  componentWillUpdate: function() {
+    console.log("Wywoływana, gdy potrzebne jest kolejne renderowanie");
+  },
+
+  componentDidUpdate: function() {
+    console.log("Sprawdzenie rzeczywistych wymiarów Dom");
+  },
+
+  componentWillUnmount: function() {
+    console.log(
+      "Unieważnianie liczników czasu, anulowanie żądań sieciowych lub czyszczenie wszystkich elementów Dom utworzonych w komponencie component Did Mount"
+    );
+  }
+});
+
+var element = React.createElement(
+  "div",
+  {},
+  React.createElement(Counter),
+  React.createElement(Counter)
+);
+
+ReactDOM.render(element, document.getElementById("app"));
